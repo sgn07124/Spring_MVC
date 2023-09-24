92d2d85ffd76a8da0d3d9d3a1223e4f725f5be8f
kimkyungho <sgn07124@gmail.com>
2023-09-24 14:54:49 +0900

    section2 - Http 응답 데이터 : 단순 텍스트, HTML, API JSON

---
+++ src/main/java/hello/servlet/basic/response/ResponseHtmlServlet.java
@@ -0,0 +1,29 @@
+package hello.servlet.basic.response;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "ResponseHtmlServlet", urlPatterns = "/response-html")
+public class ResponseHtmlServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // Content-Type: text/html; charset=utf-8
+        response.setContentType("text/html");  // 무조건 지정해줘야 함
+        response.setCharacterEncoding("utf-8");
+
+        // html응답 - 안녕? 출력됨
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<body>");
+        writer.println("  <div>안녕?</div>");
+        writer.println("</body>");
+        writer.println("</html>");
+    }
+}
